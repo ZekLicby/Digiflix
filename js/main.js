46b1bd631863518ed09fc30cf97dbfbffa0493f4
Carlos Lins <carloslinsalbertoa@gmail.com>
2022-06-20 00:06:54 -0300

    Primeiro commit (final do projeto)

---
+++ js/main.js
@@ -0,0 +1,49 @@
+$('.owl-carousel').owlCarousel({
+    loop:false,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+function clicar1(){
+    window.open("https://www.superanimes.biz/anime/digimon-adventure-dublado")
+}
+
+function clicar2(){
+    window.open("https://www.superanimes.biz/anime/digimon-adventure-2-dublado")
+}
+
+function clicar3(){
+    window.open("https://www.superanimes.biz/anime/digimon-tamers-dublado")
+}
+
+function clicar4(){
+    window.open("https://www.superanimes.biz/anime/digimon-data-squad-dublado")
+}
+
+function clicar5(){
+    window.open("https://www.superanimes.biz/anime/digimon-xros-wars-dublado")
+}
+
+function clicar6(){
+    window.open("https://www.superanimes.biz/anime/digimon-xros-wars-legendado")
+}
+
+function clicar7(){
+    window.open("")
+}
+
+function clicar8(){
+    window.open("https://www.superanimes.biz/anime/digimon-frontier-dublado")
+}
+
